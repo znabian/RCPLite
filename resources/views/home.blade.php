b5a97ba44ac00dbe11facd4aa42d9f56db976367
Zahra Nabian <zahranabian76@gmail.com>
2022-07-28 10:22:04 +0430

    change dashboard view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 text-right">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">داشبورد</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                <div class="card-body text-center">
+                    <img src="{{asset('favicon.ico')}}" style="max-height: 10rem;" class="img-fluid pb-3 rounded" alt="">
+                   <h3>
+                    سلام <b>{{auth()->user()->Name}}</b> به پنل کاخ سرخ خوش آمدید
+                   </h3>
                 </div>
             </div>
         </div>
